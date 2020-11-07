22c6503010ed34294f5b4a4b4258b6d972afaf6a
George Khoury <g@gkhoury.me>
2020-11-07 18:23:20 +0200

    Added PA01.

---
+++ W2/S3/PA01/famous_dinner.py
@@ -0,0 +1,46 @@
+# Famous Dinner
+
+# You are inviting three famous people for dinner.
+guests = ['nicola tesla', 'albert einstine', 'leonardo da vinci']
+
+# Invite your guests to dinner.
+name = guests[0].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[1].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[2].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[1].title()
+print(f"\nSorry, {name} can't make it to dinner.")
+
+# TODO: Einstine can't make it on this day! Remove him and invite Socrates instead.
+
+# TODO: Print the invitations again.
+
+# We got a bigger table
+print("\nWe got a bigger table!")
+
+# TODO: Think of three more people and add them to the list.
+# Use append() and insert() at least once
+
+# TODO: Print the invitations again.
+
+# Oh no, the table won't arrive on time!
+print("\nSorry, we can only invite two people to dinner.")
+
+# TODO: Use the following lines to remove a guest from the list, make sure you only invite two.
+#       name = guests.pop()
+#       print(f"Sorry, {name.title()} there's no room at the table.")
+
+# TODO: There should be two people left. Let's invite them.
+
+# We all have good food, and good conversation. Time to go.
+print("Good food. Good conversation. Bye for now.")
+
+# TODO: Empty out your guest list.
+
+
+# TODO: Print your empty guest list
---
+++ W2/S3/PA01/places.py
@@ -0,0 +1,24 @@
+# Places
+
+# Think of at least five places in the world you’d like to visit.
+
+# TODO: Store the locations in the list locations.
+locations = []
+
+# TODO: Print your list in its original order.
+
+# TODO:
+# Sort and print your list in alphabetical order wihout modifying the actual list. Read about the sorted() function.
+# Show that your list is still in its original order by printing it.
+
+# TODO:
+# Use sorted() to print your list in reverse alphabetical order without changing the order of the original list.
+# Show that your list is still in its original order by printing it again.
+
+# TODO: Use reverse() to change the order of your list. Print the list to show that its order has changed.
+
+# TODO: Use reverse() to change the order of your list again. Print the list to show it’s back to its original order.
+
+# TODO: Use sort() to change your list so it’s stored in alphabetical order. Print the list to show that its order has been changed.
+
+# TODO: Use sort() to change your list so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
