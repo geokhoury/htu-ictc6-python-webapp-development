3ff131c1127f335d644bfb8021561e2f761ea650
George Khoury <gkhoury@linc.local>
2020-11-14 12:33:15 +0200

    Added material for W3/S3.

--- W3/S2/function_variables.py
+++
@@ -1,9 +0,0 @@
-# Assigning Functions to Variables
-
-def hello():
-    print('Hello, World!')
-
-
-hi = hello
-hi()
-# Prints Hello, World!
--- W3/S3/CA06/shopping_list.py
+++ W3/S3/CA06/shopping_list.py
@@ -1,16 +1,45 @@
 # Shopping List
 
-# Your shopping list should keep asking for new items until nothing is entered (no input followed by enter/return key). 
+# Your shopping list should keep asking for new items until nothing is entered (no input followed by enter/return key).
 # The program should then print a menu for the user to choose one of the following options:
-# 
+# (A)dd - To add a new item to the list.
+# (F)ind - To search for an item in the list.
+# (P)rint - To pretty print the list.
+# (S)ort - To sort the list.
+# (C)lear - To clear all items in the list.
+# (Q)uit - To exit your program.
 
-# TODO: Define a data structure to keep track of your shopping list
+# TODO: Define a data structure to keep track of your shopping list.
 
+# TODO: Implement a function to show the menu to the user, then wait for a valid user choice.
+def show_menu():
+    pass
 
-# TODO: Implement a function to add an item to your shopping list
+# TODO: Implement a function to add an item to your shopping list.
 def add_item(item):
     pass
 
-# TODO: Implement a function to find an item in your shopping list
+# TODO: Implement a function to find an item in your shopping list.
 def find_item(item):
     pass
+
+# TODO: Implement a function to pretty print your tabbed lits.
+def print_list():
+    pass
+
+# TODO: Implement a function to pretty print your tabbed lits.
+def sort_list():
+    pass
+
+# TODO: Implement a function to pretty print your tabbed lits.
+def clear_list():
+    pass
+
+# TODO: Implement a function which calls the exit() function.
+def quit():
+    print("Goodbye! Hope to see you again soon :).")
+    pass
+
+
+
+
