5075a3d5924b630ed4b06a9aa7518725b8cbe6e0
George Khoury <g@gkhoury.me>
2020-11-26 10:47:09 +0200

    Added material for W5/S2/CA10/.

---
+++ W5/S2/CA10/linked_list/linked_list.py
@@ -0,0 +1,49 @@
+# Define a class for the linked list
+from node import Node
+
+# Class for a simple Linked List
+
+class LinkedList:
+    def __init__(self, data):
+        self.start_node = Node(data)
+
+    def traverse_list(self):
+        pass
+
+    def insert_at_start(self, data):
+        pass
+
+    def insert_at_end(self, data):
+        pass
+
+    def insert_after(self, x, data):
+        pass
+
+    def insert_before_item(self, x, data):
+        pass
+        
+    def insert_at_index (self, index, data):
+        pass
+
+    def get_count(self):
+        pass
+
+    def search_item(self, x):
+        pass
+
+    def make_new_list(self):
+        pass
+
+    def delete_at_start(self):
+        pass
+
+    def delete_at_end(self):
+        pass
+
+    def reverse_linkedlist(self):
+        pass
+
+
+
+# Our linked list like this now
+#   (40)->(10)->(5)->(3)-> None
---
+++ W5/S2/CA10/linked_list/node.py
@@ -0,0 +1,9 @@
+# Define a class for the tree node
+
+
+class Node:
+
+    # Define your __init__() method.
+    def __init__(self, data):
+        self.data = data
+        self.next = None
