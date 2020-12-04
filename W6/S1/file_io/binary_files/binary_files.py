e89b71df087e636e9927d0018c215fdc796d2fd1
ahmad-atmeh <73915053+ahmad-atmeh@users.noreply.github.com>
2020-12-04 13:41:42 +0200

    Update binary_files.py

--- W6/S1/file_io/binary_files/binary_files.py
+++ W6/S1/file_io/binary_files/binary_files.py
@@ -2,7 +2,7 @@
 
 #The difference between a string and a byte string is that 
 # a byte string can contain characters that a string cannot.
-
+# > used for image and PDF file 
 hw1 = "Hello, world!"
 hw2 = b"Hello, world!"
 
@@ -14,5 +14,6 @@ my_picture = open("my_picture.png", "rb")
 my_new_picture = open("my_new_picture.png", "wb")
 
 # Read and write the picture byte by byte
+# > like copy or move Command 
 for byte in my_picture:
     my_new_picture.write(byte)
